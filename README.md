c7411d4360bc4b517811cfef34a0e5d680107c90
nebbiouenf <nebbiouenf@gmail.com>
2019-08-30 21:23:39 -0300

    omicas

--- README.md
+++ README.md
@@ -5,3 +5,51 @@ Date:     Aug 15, 2019
 Nosso objetivo é ser uma ponte de comunicação entre alunos e o programa de pós-graduação em Biociências e Biotecnologia da UENF, 
 buscando promover e consolidar o conhecimento científico, tecnológico e de inovação por meio de eventos na área.
 [Link][Contact_Us.md]
+
+
+
+## Ceating a new page to the main website. 
+1. Copy the template.html file and rename it as desired page names (e.g newPage.html)  
+2. edit the line as required within the <title> tags   
+ ` <title>Template| NEBBIO</title> `
+3. go to js/scripts.js file, add one line   under the function **populate_nav_sidebar()**  
+  `h['Associações'] = 'newPage.html';`  
+  The text within h['XXX'] will be visible on the website (Associações) and newPage.html is the link to the page.   
+  
+
+## Ceating a new page to the EVENT website.     
+1. copy the 'event1' folder and rename it to desired event name (e.g omicas). Use small letters.
+2. Put the following files as per the event name  
+- fees.png : fees table  
+- header_desktop.png : page header to show on Desktop site  
+- header_mobile.png : page header to show on mobile site  
+- poster.png: poster A4 size for the event  
+3. open the index.html file.  
+` 
+# Edit the event name
+<title>Events| NEBBIO</title> 
+# Edit he background color is you want RGB and last one is opacity Alpha  . DONT change the 'event'
+<script>header_content('event','rgba(230, 255, 255, 0.005)');</script>
+`
+
+4. open the nebbiouenf.github.io/events.html page and add a link to the Event web page.
+If it is the first event in a year, you have to edit 3 things.
+`
+#i. Keep the recet year on top
+ <!-- Edit to add a new year --> 
+ <li><a href="#2019">2019</a></li>
+#ii. Add a new section of the year. Change all the 2019 to thecurrent year
+<!-- Edit to add a new year section --> 	
+      	<h3 id="2019" class="event_year">2019</h3>
+         <ol type="I">
+         </ol>
+#ii. add an event to the year section <ol> tags
+<!-- For each year section add a new event here--> 	
+           <li><a href="omicas/index.html" target='_blank' >OMICAS (Date: 03/10/2019) <img src="img/new.gif" style="height:10px; width:auto;"></a></li>
+`
+5. If it is another event in the year just do the last step (iii). Change the name and event folder name and remove the new.gif image tag from old event.
+`
+ <!-- For each year section add a new event here--> 	
+           <li><a href="genomics/index.html" target='_blank'>Genomics (Date: 22/10/2085) <img src="img/new.gif" style="height:10px; width:auto;"></a></li>
+           <li><a href="omicas/index.html" target='_blank' >OMICAS (Date: 03/10/2019) <!-- REMOVE the new.gif image --> </a></li>
+`
--- js/script.js
+++ js/script.js
@@ -9,12 +9,39 @@ function on_page_load(){
 
 //<!-- <img alt="Logo" src="img/header_desktop.png"> --> \
 //Header section
-function header_content(){
-	var header = document.getElementById("header");
-	header.innerHTML='\
-	<a id="menu" href="javascript:void(0);" onclick="show_menubar(\'.\')"><img src="img/menu_show.png" alt="menu" style="display:inline; height: 32px; width: 32px;"></a>   \
-	';
+function header_content(type="", bg_color="rgb(230,231,232)"){
+  var header = document.getElementById("header");
+  if(type == "event"){
+    header.style.backgroundColor = bg_color;
+    bg_img='header_desktop.png';
+    img_size = 'cover';
+    screen_width_avl = Screen.availWidth;
+    screen_width = window.screen.width;
+    if(screen_width >=320 && screen_width <750){ 
+      bg_img='header_mobile.png'; 
+      img_size= 'auto 90%';
+    }
+    else if(screen_width >=768 && screen_width <1024){ 
+      bg_img='header_mobile.png'; 
+      img_size= 'cover';
+    }
+    var segments = window.location.pathname.split('/');
+    // color image repeat attachment position size origin clip|initial|inherit
+    
+    header.style.backgroundImage = 'url("../'+segments.slice(-2,-1)+'/'+bg_img+'")'; 
+    header.style.backgroundSize = img_size; 
+    header.style.backgroundBlendMode= 'lighten';
+    header.innerHTML='\
+	  <a id="menu" href="javascript:void(0);" onclick="show_menubar(\'..\')"><img src="../img/menu_show.png" alt="menu1" style="display:inline; height: 32px; width: 32px;"></a>   \
+	  ';
+  }
+  else{
+  header.innerHTML='\
+	  <a id="menu" href="javascript:void(0);" onclick="show_menubar(\'.\')"><img src="img/menu_show.png" alt="menu" style="display:inline; height: 32px; width: 32px;"></a>   \
+	  ';
+  }
 }
+
 
 // Populate Navigation bar
 //  to add a new page update the hash h
@@ -25,7 +52,7 @@ function populate_nav_sidebar(){
 	h['Pagina inicial'] = 'index.html';
 //	h['Associações'] = 'membership.html';
 	h['Eventos'] = 'events.html';
-	h['Courses'] = 'courses.html';
+	h['Cursos'] = 'courses.html';
 	h['Sobre nós'] = 'about_us.html';
 	h['Contate-Nos'] = 'contact.html';	
 	var nav = document.getElementById("nav_ul");
@@ -40,15 +67,15 @@ function populate_nav_sidebar(){
 }
 
 //footer content
-function footer_content(){
+function footer_content(relative_path='.'){
 	var footer = document.getElementById("footer");
 	footer.innerHTML = '<ul> \
-       <li><a href="http://www.uenf.br/portal/index.php/br/" target="_blank"><img src="img/UENF_icon.png" style="width:40%;height:50%; " /></a></li> \
+       <li><a href="http://www.uenf.br/portal/index.php/br/" target="_blank"><img src="'+relative_path+'/img/UENF_icon.png" style=" " /></a></li> \
        <!-- <li><a href="#">Link 2</a></li> --> \
      </ul>     \
     <div style="float: right; " >\
-     <a href="https://www.facebook.com/nebbio.uenf" target="_blank"><img src="img/Facebook.png" alt="facebook icon"/></a>\
-     <a href="https://www.instagram.com/nebbiouenf/"  target="_blank" ><img src="img/Instagram.png" alt="instagram icon" /></a>\
+     <a href="https://www.facebook.com/nebbio.uenf" target="_blank"><img src="'+relative_path+'/img/Facebook.png" alt="facebook icon" class="social_me_icons"/></a>\
+     <a href="https://www.instagram.com/nebbiouenf/"  target="_blank" ><img src="'+relative_path+'/img/Instagram.png" alt="instagram icon" class="social_me_icons"/></a>\
     </div>';
 }
 //footnotes
@@ -56,9 +83,10 @@ function footNote(){
 	var footnote = document.getElementById("footnote");
 	footnote.innerHTML=' \
 	<p >Best viewed in Mozilla Firefox version 58.0 and above (with resolution 1024x768).</p> \
-	<p >Optimized view in smartphones and iPods (with resolution 375x668 and 768x1024).</p> \
+	<p >| Optimized view in smartphones and iPods (with resolution 375x668 and 768x1024).</p> \
 	';
 }
+
 // Args: relative path
 function show_menubar(path){
  var nav = document.getElementById("navigation");
@@ -74,4 +102,12 @@ function show_menubar(path){
  return(true);
 }
 
+
+//Go to top function 
+// https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
 
