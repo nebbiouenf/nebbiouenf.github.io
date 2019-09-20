7ed23efec92039b83af814a8133535a514df6e60
Kcm Laptop <Kcm Laptop>
2019-09-20 03:45:06 -0300

    count down

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ function on_page_load(){
 	//populate_nav_sidebar();
 	//footer_content();
 	//footNote();
-	//header_content();	
+	//header_content();		
 }
 
 //<!-- <img alt="Logo" src="img/header_desktop.png"> --> \
@@ -165,4 +165,32 @@ function image_slider(){
 	showSlides(slideIndex);
 	slideIndex++;	
 	setTimeout(image_slider, 5000); // Change image every 2 seconds	
+}
+
+function countdown(){
+	var countDownDate = new Date("Oct 3, 2019 08:00:00").getTime();		
+	// Get today's date and time
+	var now = new Date().getTime();
+	// Find the distance between now and the count down date
+	var distance = countDownDate - now;
+	// Time calculations for days, hours, minutes and seconds
+	var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+	var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+	// Display the result in the element with id="demo"
+	//document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+	//+ minutes + "m " + seconds + "s ";
+	document.getElementById("counter_time").innerHTML = days;
+	document.getElementById("hour").innerHTML = hours;
+	document.getElementById("minute").innerHTML = minutes;
+	document.getElementById("second").innerHTML = seconds;
+	// If the count down is finished, write some text
+	if (distance < 0) {		
+		document.getElementById("counter_time").innerHTML = "-1";
+		document.getElementById("hour").innerHTML = 0;
+		document.getElementById("minute").innerHTML = 0;
+		document.getElementById("second").innerHTML = 0;
+	  }
 }
